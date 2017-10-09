adc12d00549c37dc0dc548673ebdc57b493e5084
wolfgang <wolfgang@wolfgangdeutsch.work>
2017-10-09 11:03:00 +0200

    Create function assert_equal

--- test.sh
+++ test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+function assert_equal
+{
+  actual=$1
+  execpted=$2
+  if [ "$actual" != "$expected" ]; then
+    echo "FAILURE!"
+    echo "Expected: $expected"
+    echo "But got:  $actual"
+    exit -1
+  else
+    echo "SUCCESS!"
+  fi
+}
 
 stack test
 [ "$?" != "0" ] && exit $?
@@ -7,26 +20,12 @@ MYWC="stack exec wc --"
 echo "---- Running end-to-end tests"
 echo ""
 
-output=$($MYWC -l ./test_files/3_lines.txt)
-expected=$(wc -l ./test_files/3_lines.txt)
+output="$($MYWC -l ./test_files/3_lines.txt)"
+expected="$(wc -l ./test_files/3_lines.txt)"
 
-if [ "$output" != "$expected" ]; then
-  echo "FAILURE!"
-  echo "Expected: $expected"
-  echo "But got:  $output"
-  exit -1
-else
-  echo "SUCCESS!"
-fi
+assert_equal "$output" "$expected"
 
 output=$($MYWC -l ./test_files/4_lines.txt)
 expected=$(wc -l ./test_files/4_lines.txt)
 
-if [ "$output" != "$expected" ]; then
-  echo "FAILURE!"
-  echo "Expected: $expected"
-  echo "But got:  $output"
-  exit -1
-else
-  echo "SUCCESS!"
-fi
+assert_equal "$output" "$expected"
