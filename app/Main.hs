8dd1160cf7195d75f09bb0a13e5707f5444d6eaa
wolfgang <wolfgang@wolfgangdeutsch.work>
2017-10-09 15:42:35 +0200

    Use WcEval in main

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,12 @@ module Main where
 
 import System.Environment
 import qualified WcArgs
+import qualified WcEval
 
 main :: IO ()
 main = do
       args <- getArgs
       let fileName = WcArgs.getFileName args
       contents <- readFile fileName
-      let lineCount = getLineCount contents
+      let lineCount = WcEval.getLineCount contents
       putStrLn $ (show lineCount) ++ " " ++ fileName
-
-getLineCount contents = (length $ lines contents)
