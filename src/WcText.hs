14e42f9ed31602597ea81cc0e7eb79fed0b875c5
wolfgang <wolfgang@wolfgangdeutsch.work>
2017-10-10 17:02:02 +0200

    Implement WcText.getWordCount

--- src/WcText.hs
+++ src/WcText.hs
@@ -1,4 +1,7 @@
-module WcText (getLineCount) where
+module WcText (getLineCount, getWordCount) where
 
 getLineCount :: String -> Int
 getLineCount text = length $ lines text
+
+getWordCount :: String -> Int
+getWordCount text = length $ words text
--- test/WcTextSpec.hs
+++ test/WcTextSpec.hs
@@ -11,3 +11,10 @@ spec = describe "WcText" $ do
       WcText.getLineCount "line1" `shouldBe` 1
     it "should return 3 for three lines" $ do
       WcText.getLineCount "line1\nline2\nline3" `shouldBe` 3
+  describe "getWordCount returns number of words" $ do
+    it "should return 1 for a line with a single word" $ do
+      WcText.getWordCount "word" `shouldBe` 1
+    it "should return 3 for a line with three words" $ do
+      WcText.getWordCount "word1 word2 word3" `shouldBe` 3
+    it "should return 5 for 2 lines with five words and some other whitespace" $ do
+      WcText.getWordCount "word1   word2\tword3\nword4    word5  " `shouldBe` 5
