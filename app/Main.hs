82eb0e9d2cd5a376ce5e0fc308aa6586e4407b07
wolfgang <wolfgang@wolfgangdeutsch.work>
2017-10-11 16:46:09 +0200

    Clean up Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -8,20 +8,21 @@ import qualified WcText
 main :: IO ()
 main = do
       args <- getArgs
+      let fileName = WcArgs.getFileName args
       case WcArgs.getAction args of
         WcArgs.CountLines -> do
-          let fileName = WcArgs.getFileName args
-          contents <- readFile fileName
-          let lineCount = WcText.getLineCount contents
-          putStrLn $ buildOutput lineCount fileName
+          processFile fileName WcText.getLineCount
         WcArgs.CountWords -> do
-          let fileName = WcArgs.getFileName args
-          contents <- readFile fileName
-          let wordCount = WcText.getWordCount contents
-          putStrLn $ buildOutput wordCount fileName
+          processFile fileName WcText.getWordCount
         WcArgs.InvalidOption option -> do
           hPutStrLn stderr $ "wc: invalid option -- '" ++ option ++ "'"
           hPutStrLn stderr "Try 'wc --help' for more information."
+
+processFile :: String -> (String -> Int) -> IO ()
+processFile fileName processor = do
+  contents <- readFile fileName
+  let count = processor contents
+  putStrLn $ buildOutput count fileName
 
 buildOutput :: Int -> String -> String
 buildOutput prefix fileName = (show prefix) ++ " " ++ fileName
